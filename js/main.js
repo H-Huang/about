f00424541e65d05476cf8aaacfc4661e9d8e75e0
Howard Huang <howardhuang96@gmail.com>
2017-07-06 23:11:37 -0700

    class change trigger

--- js/main.js
+++ js/main.js
@@ -8,10 +8,28 @@ $(document).ready(function() {
 
     // Create scene
     $("section").each(function() {
+
+        var name = $(this).attr('id');
         new ScrollMagic.Scene({
             triggerElement: this
         })
         .setPin(this)
+        .addIndicators({
+            colorStart: "rgba(255,255,255,0.5)",
+            colorEnd: "rgba(255,255,255,0.5)", 
+            colorTrigger : "rgba(255,255,255,1)",
+            name:name
+        })
         .addTo(ctrl);
     });
+
+    // get window height
+    var wh = window.innerHeight;
+    console.log(wh);
+
+    new ScrollMagic.Scene({
+        offset: wh*3 - 1
+    })
+    .setClassToggle("#four", "is-active")
+    .addTo(ctrl);
 });
